2d15f5606e0561ac5a17cf8f6bd82b825e5d6a72
Sahil Kapadia <sahilkapadia89@gmail.com>
2023-09-30 20:02:28 +0530

    2nd day of learning JS

---
+++ lct-3/main.js
@@ -0,0 +1,39 @@
+function checkBlank(c1, msg) {
+
+    data = c1.value;
+
+    // alert(data);
+    l = data.length;
+    
+    if (l == 0) {
+        alert(msg);
+        return false;
+    }
+    return true;
+}
+
+function checkPassLen(c1, msg) {
+
+    x = c1.value;
+    l = x.length;
+
+    if (l < 8) {
+        alert(msg);
+        c1.focus();
+        return false;
+    }
+    return true;
+}
+
+function checkPassConfPass(c1, c2, msg) {
+    x = c1.value;
+    y = c2.value;
+    if (x != y) {
+        alert(msg);
+        c1.value = "";
+        c2.value = "";
+        c1.focus();
+        return false;
+    }
+    return true;
+}
